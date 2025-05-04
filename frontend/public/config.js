0035e8b45b7a59ba46ba003d7c18ecacd0bee046
Ayush Sahu <114695041+Magacrk@users.noreply.github.com>
2025-05-05 04:33:25 +0530

    Update config.js

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -4,7 +4,8 @@ window.APP_CONFIG = {
   AWS_REGION: 'us-east-1',
   AWS_COGNITO_USER_POOL_ID: 'us-east-1_RKkNIPIl8', // Replace with your actual value
   AWS_COGNITO_CLIENT_ID: '724f1dh164tvo2v52udd137ffs', // Replace with your actual value
-  AWS_IDENTITY_POOL_ID: 'us-east-1:dad9276d-4cd6-4905-be0d-3cf55d5b3b58'
+  AWS_IDENTITY_POOL_ID: 'us-east-1:dad9276d-4cd6-4905-be0d-3cf55d5b3b58',
+  VITE_USER_POOL_CLIENT_ID: '724f1dh164tvo2v52udd137ffs',
   BACKEND_URL: 'https://http://ecommerce-backend-prod.eba-m93ahsjr.us-east-1.elasticbeanstalk.com/',
   DEBUG_MODE: true
 }; 
