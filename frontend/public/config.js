e324eff67bb23f6e783c78e13f083727ac3448ea
Ayush Sahu <114695041+Magacrk@users.noreply.github.com>
2025-05-05 04:14:46 +0530

    Update config.js

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,10 +1,9 @@
-// Direct configuration file for diagnosing environment issues
 console.log('Config.js loaded');
 
 window.APP_CONFIG = {
   AWS_REGION: 'us-east-1',
-  AWS_COGNITO_USER_POOL_ID: 'us-east-1_xxxxxxxx', // Replace with your actual value
-  AWS_COGNITO_CLIENT_ID: 'xxxxxxxxxxxxxxxxxxxxxxxxxx', // Replace with your actual value
-  BACKEND_URL: 'https://your-backend-url.com',
+  AWS_COGNITO_USER_POOL_ID: 'us-east-1_vNjSgFwRL', // Replace with your actual value
+  AWS_COGNITO_CLIENT_ID: '25ak6jbiib0vl2168o4jl1gc3f', // Replace with your actual value
+  BACKEND_URL: 'https://http://ecommerce-backend-prod.eba-m93ahsjr.us-east-1.elasticbeanstalk.com/',
   DEBUG_MODE: true
 }; 
